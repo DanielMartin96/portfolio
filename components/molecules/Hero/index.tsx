243d4e7c6b840f6c15b9b8ba118d90cc25d815d9
Daniel Martin <daniel.martin@rentokil-initial.com>
2022-03-21 21:47:44 +0000

    added routes, layout and contact page

--- components/molecules/Hero/index.tsx
+++ components/molecules/Hero/index.tsx
@@ -2,14 +2,14 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <section className="bg-gray-800 h-screen container px-6 py-16 mx-auto">
+    <section className="bg-gray-800 h-screen px-6 py-16 mx-auto">
       <div>
         <h1 className="text-white">Hi, my name is</h1>
         <h2 className="text-3xl font-bold text-white md:text-7xl">
           Daniel Martin.
         </h2>
         <h3 className="text-3xl font-bold text-gray-400 md:text-7xl">
-          I build things for the web.
+          I love building things for the web.
         </h3>
         <p className="mt-6 text-gray-300 max-w-lg">
           I’m a software engineer specializing in building exceptional digital
