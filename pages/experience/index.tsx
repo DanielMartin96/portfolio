4a35d50709c45985986ca12af75a9a6b805d499e
Daniel Martin <daniel.martin@rentokil-initial.com>
2022-03-21 22:16:05 +0000

    Added timeline component

--- pages/experience/index.tsx
+++ pages/experience/index.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import Timeline from "../../components/molecules/Timeline";
 
 const Experience = () => {
-  return <div>Experience</div>;
+  return (
+    <section className="px-6 py-4 bg-gray-800">
+      <h1 className="text-4xl text-white font-bold my-10">My Experience</h1>
+      <Timeline />
+    </section>
+  );
 };
 
 export default Experience;
--- utils/constants/index.ts
+++ utils/constants/index.ts
@@ -1,1 +1,22 @@
 export const NAV_LINKS = ["About", "Experience", "Work", "Contact"];
+
+export const EXPERIENCE = [
+  {
+    title: "Team Treehouse Tech Degree",
+    role: "Self Taught",
+    date: "July 2020 - December 2020",
+    description: "example",
+  },
+  {
+    title: "Dynamic Sales Solutions",
+    role: "Junior Web Developer",
+    date: "January 2021 - June 2021",
+    description: "example",
+  },
+  {
+    title: "Cognizant",
+    role: "React Developer",
+    date: "July 2021 - Present",
+    description: "example",
+  },
+];
